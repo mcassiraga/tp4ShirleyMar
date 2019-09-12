762cb7246c4e7b13acb068de165a33274359cf64
mcassiraga <mcassiraga@protonmail.com>
2019-09-12 19:47:06 -0300

    modal no funciona. agregué nodemon

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,8 @@
 console.log('estoy bien linkeado')
+
+    function toggleModal() {
+        var modal = document.querySelector(".modal");
+        var closeButton = document.querySelector(".close-button");
+        modal.classList.toggle("show-modal");
+        closeButton.addEventListener("click", toggleModal);
+    }
