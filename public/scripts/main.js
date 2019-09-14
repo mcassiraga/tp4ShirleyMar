46e5c88a2f621b1d08d211604a3c43c9ebb9c2de
mcassiraga <mcassiraga@protonmail.com>
2019-09-14 13:56:48 -0300

    estilos del modal

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,16 @@
 console.log('estoy bien linkeado')
 
-    function toggleModal() {
-        var modal = document.querySelector(".modal");
-        var closeButton = document.querySelector(".close-button");
-        modal.classList.toggle("show-modal");
-        closeButton.addEventListener("click", toggleModal);
+const toggleModal = () => {
+    let modal = document.querySelector(".modal");
+    let closeButton = document.querySelector(".close-button");
+    modal.classList.toggle("show-modal");
+    
+    function windowOnClick(event) {
+        if (event.target === modal) {
+            toggleModal();
+        }
     }
+
+    closeButton.addEventListener("click", toggleModal);
+    window.addEventListener("click", windowOnClick);
+}
